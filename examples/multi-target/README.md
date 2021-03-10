e9aa4c9cfc5cd7700f8838113daa111a3a85cfd5
Kyunghwan Kwon <kwon@toanyone.net>
2021-03-10 18:41:17 +0900

    example: add multi-target project

---
+++ examples/multi-target/README.md
@@ -0,0 +1,5 @@
+## Build
+### nrf52
+`make nrf52`
+### esp32
+`make esp32`
---
+++ examples/multi-target/include/sleep.h
@@ -0,0 +1,14 @@
+#ifndef SLEEP_H
+#define SLEEP_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+void sleep_ms(unsigned int msec);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* SLEEP_H */
